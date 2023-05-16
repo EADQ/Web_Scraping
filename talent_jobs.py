d8b5680a3db3baff97d5b5d8fbeb5b9248e4b053
EADQ <easoftwareengineer@gmail.com>
2023-05-15 23:52:23 -0600

    scrapeando otra web

--- talent_jobs.py
+++ talent_jobs.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import random
 
 def extract(page):
-    url = f'https://cr.indeed.com/jobs?q=&l=Costa+Rica&start={page}&pp=gQAPAAAAAAAAAAAAAAACAzfamgAmAQEBBgbtgRxJLmlxnE8yAOiPH4_PJw73BEvM92GC3fM_Sc8iPnwAAA'
+    url = f'https://cr.talent.com/jobs?k=Software+Engineer&l=Costa+Rica&radius=15&p={page}&context=serp_pagination'
     user_agents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
@@ -12,8 +12,19 @@ def extract(page):
         'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'
     ]
     r = requests.get(url, headers={'User-Agent': random.choice(user_agents)})
-    return r.status_code
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
 
-print(extract(0))
+def transform(soup):
+    divs = soup.find_all('div', class_ ='card card__job')
+    for item in divs:
+        tittle = item.find('a').text.strip()
+        company = item.find('div', class_='card__job-empname-label').text.strip()
+        # description = soup.select_one("jobsPreview")
+        description = item.find('div', id='jobPreview__body--description').text.strip() 
+        # description = item.find('div', {'class' : 'card card--preview acsb-active'}).text.strip()
+        print(description)
 
-# Investigar para solucionar el estado de 403 que estamos teniendo para poder continuar
+c = extract(0)
+transform(c)
+
